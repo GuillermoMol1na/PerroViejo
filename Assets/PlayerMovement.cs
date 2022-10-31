756a942eb223bd630304ecb9d49f93786f497936
GuillermoMol1na <guillermo.m@krugerns.com>
2022-10-31 16:26:27 -0400

    Colisiones habitacion
    Descomposición en bloques de la habitación y la adición de sus colisionadores en 2d

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,22 +4,58 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
- public float moveSpeed = 5f;
+     public Rigidbody2D rb; 
+     Vector2 movement;
 
- public Rigidbody2D rb; 
- Vector2 movement;
+      public float moveSpeed = 0.2f;
+      public Animator anim;
+
+    // Start is called before the first frame update
+
+ 
 
     // Update is called once per frame
     void Update()
     {
-        // input
+
+
         movement.x =Input.GetAxisRaw("Horizontal");
         movement.y =Input.GetAxisRaw("Vertical");
+
+
+        if (movement.x > 0.01f || movement.x < -0.01f)
+         {
+             movement.y = 0;
+             anim.SetBool("isMoving", true);
+             anim.SetFloat("Horizontal", movement.x);
+             //anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+             anim.SetFloat("Vertical", movement.y);
+             
+         }
+         else if ( movement.y > 0.01f || movement.y < -0.01f)
+         {
+             movement.x = 0;
+             anim.SetBool("isMoving", true);
+             anim.SetFloat("Horizontal", movement.x);
+             //anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+             anim.SetFloat("Vertical", movement.y);
+        }
+        else{
+            movement.x = 0;
+            movement.y = 0;
+            anim.SetFloat("Horizontal", movement.x);
+            anim.SetFloat("Vertical", movement.y);
+            anim.SetBool("isMoving", false);
+        }
+        
+        
     }
 
     void FixedUpdate(){
         //Movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
+             
+          
+    rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
